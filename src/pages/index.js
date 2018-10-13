0269a405f279dc139fa36dc0521a5f1f4506c16b
furus <furusu0715@gmail.com>
2018-10-13 18:19:02 +0900

    Gatsbyjs

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Text = styled.p`
+    color:red
+    `
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <Text>Welcome to your new Gatsby site.</Text>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
