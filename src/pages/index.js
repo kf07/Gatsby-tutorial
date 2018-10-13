0495dc17650437557b8c6abba7bdd0c4c12e783e
furus <furusu0715@gmail.com>
2018-10-13 18:25:54 +0900

    Gatsbyjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Text = styled.p`
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>Hi peoples</h1>
     <Text>Welcome to your new Gatsby site.</Text>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
